d2aff8744e7358a4738d387e8e2a69d6a7c6166c
richardyzhu <richard.yunqi.zhu@gmail.com>
2024-09-27 00:38:34 -0400

    added chips

--- src/sections/ExperienceSection.tsx
+++ src/sections/ExperienceSection.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ExperienceCard from "../components/ExperienceCard";
-import { FaReact, FaAws, FaNode } from "react-icons/fa"; // Example icons
+import { FaReact, FaAws, FaNode } from "react-icons/fa";
+import { RiNextjsFill, RiVuejsFill } from "react-icons/ri";
+import { SiSelenium, SiRedux, SiApachecouchdb } from "react-icons/si";
 
 const ExperienceSection: React.FC = () => {
   const experiences = [
@@ -9,14 +11,23 @@ const ExperienceSection: React.FC = () => {
       date: "Jan 2024 - May 2024 ⠀⠀ Sep 2024 - Dec 2024",
       role: "Full Stack Software Engineering Intern",
       bulletPoints: [
-        "Refactored *AWS DynamoDB* schema using Python, resulting in a 99% reduction in monthly AWS costs while ensuring scalability through a transition from relational to non-relational structure.",
-        "Overhauled React frontend by building an internal component library based on RadixUI, resulting in a 90% reduction in bundle size and reducing load speeds by up to 45%.",
-        "Wrote a Selenium-based Yellow Pages scraper to improve customer outreach, surpassing a previous scraper's coverage by 40% and reducing its runtime by 50%.",
+        "Refactored AWS DynamoDB schema using Python, resulting in a *99% reduction* in monthly AWS costs while ensuring scalability through a transition from relational to non-relational structure.",
+        "Overhauled React frontend by building an internal component library based on RadixUI, resulting in a *90% reduction* in bundle size and *reducing load speeds* by up to *45%*.",
+        "Wrote a Selenium-based Yellow Pages scraper to improve customer outreach, surpassing a previous scraper's coverage by *40%* and reducing its runtime by *50%*.",
       ],
       location: "Toronto, Canada",
       chips: [
+        <span className="bg-black text-white text-xs font-semibold px-3 py-1 rounded-full flex items-center">
+          <RiNextjsFill className="mr-2" /> Next.js
+        </span>,
         <span className="bg-blue-500 text-white text-xs font-semibold px-3 py-1 rounded-full flex items-center">
           <FaReact className="mr-2" /> React
+        </span>,
+        <span className="bg-orange-500 text-white text-xs font-semibold px-3 py-1 rounded-full flex items-center">
+          <FaAws className="mr-2" /> AWS
+        </span>,
+        <span className="bg-green-500 text-white text-xs font-semibold px-3 py-1 rounded-full flex items-center">
+          <SiSelenium className="mr-2" /> Selenium
         </span>,
       ],
     },
@@ -31,8 +42,20 @@ const ExperienceSection: React.FC = () => {
       ],
       location: "Markham, Canada",
       chips: [
+        <span className="bg-green-500 text-white text-xs font-semibold px-3 py-1 rounded-full flex items-center">
+          <RiVuejsFill className="mr-2" /> Vue
+        </span>,
         <span className="bg-blue-500 text-white text-xs font-semibold px-3 py-1 rounded-full flex items-center">
           <FaReact className="mr-2" /> React
+        </span>,
+        <span className="bg-purple-500 text-white text-xs font-semibold px-3 py-1 rounded-full flex items-center">
+          <SiRedux className="mr-2" /> Redux
+        </span>,
+        <span className="bg-green-600 text-white text-xs font-semibold px-3 py-1 rounded-full flex items-center">
+          <FaNode className="mr-2" /> Node.js
+        </span>,
+        <span className="bg-red-500 text-white text-xs font-semibold px-3 py-1 rounded-full flex items-center">
+          <SiApachecouchdb className="mr-2" /> CouchDB
         </span>,
       ],
     },
