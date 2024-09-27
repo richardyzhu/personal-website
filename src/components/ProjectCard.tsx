b1e8b1fc9301acf3bb83e0192f3155fe43595be1
richardyzhu <richard.yunqi.zhu@gmail.com>
2024-09-27 00:37:58 -0400

    added to projects

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,28 +1,82 @@
-import React from "react";
+"use client";
+import Image from "next/image";
+import { FaReact } from "react-icons/fa"; // Example icon for a chip
+import React, { ReactNode } from "react";
+import { MdOpenInNew } from "react-icons/md";
 
 interface ProjectCardProps {
   title: string;
   image: string;
   bulletPoints: string[];
+  chips: ReactNode[];
+  awards?: string[];
+  link: string;
 }
 
 const ProjectCard: React.FC<ProjectCardProps> = ({
   title,
   image,
   bulletPoints,
+  chips,
+  awards,
+  link,
 }) => {
+  const renderBulletPoint = (point: string) => {
+    const parts = point.split(/(\*[^*]+\*)/g);
+    return parts.map((part, index) => {
+      if (part.startsWith("*") && part.endsWith("*")) {
+        return (
+          <strong key={index} className="font-bold">
+            {part.slice(1, -1)}
+          </strong>
+        );
+      }
+      return part;
+    });
+  };
   return (
-    <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg transition-all duration-300 ease-in-out hover:shadow-2xl hover:scale-105">
-      <img src={image} alt={title} className="w-full h-48 object-cover" />
-      <div className="p-6">
-        <h3 className="text-xl font-semibold text-white mb-4">{title}</h3>
-        <ul className="list-disc list-inside space-y-2 text-gray-300">
-          {bulletPoints.map((point, index) => (
-            <li key={index} className="text-sm">
-              {point}
+    <div className="bg-gray-800 rounded-lg shadow-md border border-gray-700 overflow-hidden transition-transform duration-300 ease-in-out hover:shadow-lg hover:scale-105">
+      <div className="relative">
+        <img
+          src={image}
+          alt={title}
+          className="w-full h-60 object-cover rounded-t-lg"
+        />
+      </div>
+
+      <div className="py-6 px-8 ">
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col">
+            <h3 className="text-2xl font-bold text-white mb-1">{title}</h3>
+            {awards && awards.length > 0 && (
+              <h4 className="text-sm font-semibold text-blue-400">
+                {awards.join(", ")}
+              </h4>
+            )}
+          </div>
+          {link && (
+            <button
+              onClick={() => window.open(link, "_blank")}
+              className="bg-indigo-600 text-white font-semibold py-2 px-3 rounded-full transition duration-200 hover:bg-indigo-500 flex items-center space-x-2"
+            >
+              <MdOpenInNew />
+            </button>
+          )}
+        </div>
+
+        <hr className="border-gray-600 my-3" />
+
+        <ul className="list-disc list-outside space-y-2 text-gray-300">
+          {bulletPoints.map((item, index) => (
+            <li key={index} className="text-sm leading-relaxed">
+              {renderBulletPoint(item)}
             </li>
           ))}
         </ul>
+
+        <div className="flex flex-wrap gap-3 justify-start items-center mt-4">
+          {chips && chips.map((chip, index) => <div key={index}>{chip}</div>)}
+        </div>
       </div>
     </div>
   );
--- src/sections/ProjectsSection.tsx
+++ src/sections/ProjectsSection.tsx
@@ -3,40 +3,41 @@ import ProjectCard from "../components/ProjectCard";
 const ProjectsSection: React.FC = () => {
   const projects = [
     {
-      title: "AI Chatbot",
-      image: "/api/placeholder/400/300",
+      title: "MotiSpectra",
+      image: "/images/motispectra.png",
       bulletPoints: [
-        "Developed using OpenAI's GPT-3 API",
-        "Implemented context-aware responses",
-        "Integrated with Discord for easy access",
+        "Developed a real-time attentiveness and emotion analysis software using Next.js and Python to seamlessly integrate with virtual call platforms (Zoom, Google Meet, MS Teams).",
+        "Trained a CNN on the FER-2013 dataset using TensorFlow to achieve a 70% accuracy, used data augmentation and ensemble learning to train a separate model on an attentiveness dataset, achieving a 67% accuracy",
       ],
+      chips: [],
+      awards: ["2nd Overall at ConUHacks VIII"],
     },
     {
-      title: "E-commerce Platform",
-      image: "/api/placeholder/400/300",
+      title: "NarratorRL",
+      image: "/images/narratorrl.png",
       bulletPoints: [
-        "Built with React and Node.js",
-        "Integrated Stripe for secure payments",
-        "Implemented responsive design for mobile users",
+        "Built a mobile app using React Native, Django, and Tesseract OCR that detects words in images and provides audio narration, empowering individuals with visual impairments.",
+        "Implemented advanced features with the Cohere API including text summarization and language detection. Trained two custom NLP models to verify the legibility of text and to summarize text into keywords.",
       ],
+      chips: [],
+      awards: ["1st Place Cohere Challenge, 2nd Overall at MetHacks"],
     },
     {
-      title: "Data Visualization Dashboard",
-      image: "/api/placeholder/400/300",
+      title: "Review Recap",
+      image: "/images/reviewrecap.png",
       bulletPoints: [
-        "Created using D3.js and React",
-        "Real-time data updates via WebSocket",
-        "Customizable charts and graphs",
+        "Built a Chrome extension in JavaScript for Amazon product pages to help users make informed decisions.",
+        "Implemented multi-threading with Beautiful Soup in Python to scrape 5000+ reviews in seconds.",
+        "Cached results using a RESTful Django backend.",
       ],
+      chips: [],
+      awards: ["1st Place Noibu Challenge at uOttaHack V"],
     },
     {
-      title: "Mobile Fitness App",
-      image: "/api/placeholder/400/300",
-      bulletPoints: [
-        "Developed with React Native",
-        "Integrated with health tracking APIs",
-        "Implemented gamification features for user engagement",
-      ],
+      title: "Coming Soon...",
+      image: "/images/wip.png",
+      bulletPoints: [],
+      chips: [],
     },
   ];
 
